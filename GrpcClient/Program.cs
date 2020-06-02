e6d2164ebbe3a01184322962a5c889011c72efa8
wkang <wkang@heygears.com>
2020-06-02 14:30:47 +0800

    项目创建

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,42 @@
+﻿using Grpc.Net.Client;
+using grpc_netcore_demo;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                
+                AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);//跨语言调用
+
+                //访问项目自身的.net core grpc服务
+                var channel = GrpcChannel.ForAddress("http://localhost:5001");
+                var client = new Greeter.GreeterClient(channel);
+                var reply = await client.SayHelloAsync(
+                    new HelloRequest { Name = "World" });
+                Console.WriteLine(reply.Message);
+
+                //跨语言访问 golang grpc服务
+                var channel_go = GrpcChannel.ForAddress("http://localhost:50051");
+                var client_go = new Greeter.GreeterClient(channel_go);
+                var reply_go = await client_go.SayHelloAsync(
+                    new HelloRequest { Name = "World" });
+                Console.WriteLine(reply_go.Message);
+                Console.ReadKey();
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+            }
+            
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # grpc-netcore-demo
 一个基于.net core 3.0的gRpc客户端和服务端，并支持跨语言调用Go gRpc服务端
+
+## 项目架构
+### GrpcClient
+
+gRpc客户端，访问了项目本身的gRpc服务，并测试访问了Go语言开发的gRpc服务，实现跨语言通信   
+Golang开发的gRpc服务源码[https://github.com/flyingkoala/grpc-go-demo](https://github.com/flyingkoala/grpc-go-demo "点击这里")
+
+### GrpcService
+
+gRpc服务端   
+
+RPC是基于http/2，是同时支持https和http协议的，走http协议，需如下配置Program.cs   
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.ConfigureKestrel(options =>
+                    {
+                        // Setup a HTTP/2 endpoint without TLS.
+                        options.ListenLocalhost(5001, o => o.Protocols =
+                            HttpProtocols.Http2);
+                    });
+                    webBuilder.UseStartup<Startup>();
+                });
+   同时，客户端请求时需设置   
+
+    AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
